cf81a31ca059b043b0a7b15ac85e2132ddb99b89
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-01-21 00:35:28 -0500

    Registered signal handlers for graceful shutdown

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { close } from "../src/redis.js";
 
+if(process.env.NEXT_MANUAL_SIG_HANDLE) {
+    process.on("SIGINT", async () => {
+        if(process.env.REDIS?.includes("rue")) {
+            console.log("Closing Redis connection...");
+            await close();
+            console.log("Closed.");
+        }
+        console.log("Done.");
+    });
+    
+    process.on("SIGTERM", async () => {
+        if(process.env.REDIS?.includes("rue")) {
+            console.log("Closing Redis connection...");
+            await close();
+            console.log("Closed.");
+        }
+        console.log("Done.");
+    })
+}
+
 export default function Document() {
   return (
     <Html lang="en">
