f7216983d0071ed3143f8d72f1d9503de98932a3
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-01-21 22:40:11 -0500

    Changed log file permissions to 640

--- src/logger.ts
+++ src/logger.ts
@@ -6,7 +6,7 @@ const baseLoggingDirectory = process.env.LOGGING_DIRECTORY || path.join(process.
 async function metaLog(name: "database" | "authentication", message: string) {
     const filePath = path.join(baseLoggingDirectory, name, logFileNameFromDate());
     message = (new Date()).toUTCString() + "\n" + message + "\n\n";
-    await fs.appendFile(filePath, message, { mode: 0o750, flag: "a+" });
+    await fs.appendFile(filePath, message, { mode: 0o640, flag: "a" });
 }
 
 // Logs a message related to a specific vault.
@@ -17,7 +17,7 @@ async function vaultLog(vaultName: string, message: string) {
     // TODO: Switch to file descriptors and have array of file
     // descriptors to read and write to. (Performance reasons)
     message = (new Date()).toUTCString() + "\n" + message + "\n\n";
-    await fs.appendFile(filePath, message, { mode: 0o750, flag: "a+" });
+    await fs.appendFile(filePath, message, { mode: 0o640, flag: "a" });
 }
 
 // Returns the name of the log file associated with the current date.
