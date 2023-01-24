c8e147da6ecb81bfcfa0732ce3e7d5a52f5fab05
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-01-24 10:47:18 -0500

    Authentication module + tests

---
+++ src/auth_test.ts
@@ -0,0 +1,93 @@
+import { after, describe, it } from "node:test";
+import assert from "assert";
+
+import { close } from "./redis.js";
+
+let status = 0;
+
+async function shutdown() {
+    console.log("Closing Redis connection...");
+    await close();
+    console.log("Closed.");
+    
+    console.log("Done.");
+}
+
+process.on("SIGINT", shutdown);
+
+import JWT from "./jwt.js";
+
+process.env.JWT_SECRET = "4B6576696E20697320636F6F6C";
+process.env.DOMAIN = "Kevin";
+if(process.env.REDIS) console.log("Using Redis");
+else console.log("Using in memory database");
+
+const { isValidToken, createToken, addNewVaultToToken, removeVaultFromToken, outdateToken, refreshTokenExpiration } = await import("./authentication.js");
+
+describe("Testing authentication module which uses Redis", () => {
+    it("Creates a new token and checks if successful by using the JWT class", () => {
+        const token = createToken(["main_vault"]);
+        const [_header, payload] = JWT.unwrap(token, process.env.JWT_SECRET as string);
+        const token2 = new JWT(process.env.DOMAIN as string, payload.exp, payload.iat)
+                .addClaim("vaults", ["main_vault"])
+                .addClaim("nonce", payload.nonce)
+                .getToken(process.env.JWT_SECRET);
+        assert(token === token2);
+        assert(isValidToken(token));
+        assert(isValidToken(token2));
+    });
+    
+    it("Checks that expired tokens and bad tokens are not valid", async () => {
+        const token = createToken(["vault"]);
+        assert(await isValidToken(token));
+        assert(!await isValidToken(token.substring(0, token.length - 1)));
+        
+        // Taken from main tests. exp was at 1000000000 unix time (Sept 9, 2001) (A long time ago)
+        const oldToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJLZXZpbiIsImV4cCI6MTAwMDAwMDAwMCwiaWF0IjoxMTExMTExMTExLCJpc3N1ZXJJc0Nvb2wiOnRydWV9.wm1_FGup-Jkj8b9_EtV0sLWc8Z-xkW2sIq0y48TaJiQ";
+        assert(!await isValidToken(oldToken));
+    });
+    
+    it("Outdates a token", async () => {
+        const token = createToken(["log_me_out"]);
+        const [_header, payload] = JWT.unwrap(token, process.env.JWT_SECRET as string);
+        outdateToken(token, payload.exp);
+        assert(!await isValidToken(token));
+    });
+    
+    it("Creates a new token based on an old token with new vault", () => {
+        const token = createToken(["old_vault"]);
+        const newToken = addNewVaultToToken(token, "new_vault");
+        const [_header, payload] = JWT.unwrap(newToken, process.env.JWT_SECRET as string);
+        assert(payload.vaults.includes("old_vault"));
+        assert(payload.vaults.includes("new_vault"));
+    });
+    
+    it("Creates a new token based on an old token with a vault removed", () => {
+        const token = createToken(["remove_me"]);
+        const newToken = removeVaultFromToken(token, "remove_me");
+        const [_header, payload] = JWT.unwrap(newToken, process.env.JWT_SECRET as string);
+        assert(!payload.vaults.includes("remove_me"));
+        assert(!payload.vaults.includes("random"));
+        assert(!payload.vaults.includes(""));
+    });
+    
+    it("Refreshes a token's expiration date", async () => {
+        const token = createToken(["log_me_out"]);
+        const [_header, payload] = JWT.unwrap(token, process.env.JWT_SECRET as string);
+        assert(await isValidToken(token));
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
+        const newToken = refreshTokenExpiration(token);
+        const [_newHeader, newPayload] = JWT.unwrap(newToken, process.env.JWT_SECRET as string);
+        assert(newPayload.exp > payload.exp);
+        assert(newPayload.iat > payload.iat);
+        assert(!await isValidToken(token));
+    });
+    
+    after(() => status++);
+});
+
+while(status !== 1) {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+}
+shutdown();
---
+++ src/authentication.ts
@@ -0,0 +1,84 @@
+import JWT from "./jwt.js";
+import { localAddOutdatedToken as __localAddOutdatedToken, localIsOutdatedToken as __localIsOutdatedToken } from "./database.js";
+import { redisAddOutdatedToken as __redisAddOutdatedToken, redisIsOutdatedToken as __redisIsOutdatedToken } from "./redis.js";
+import { metaLog } from "./logger.js";
+import { unixTime } from "./helper.js";
+
+const tokenExpirationTime = parseInt(process.env.JWT_EXPIRATION as string) || 7 * 24 * 60 * 60; // Default is 1 week
+const issuer = process.env.DOMAIN !== undefined ? process.env.DOMAIN
+    : (() => { throw new Error(`The DOMAIN environment variable must be specified. (It is used as the "issuer" field in the JSON Web Token.)`); })();
+const secret = process.env.JWT_SECRET !== undefined ? process.env.JWT_SECRET
+    : (() => { throw new Error("The JWT_SECRET environment variable must be specified."); })();
+if(!(/^[a-fA-F0-9]+$/.test(secret))) throw new Error("Secret must be a hex string. (No 0x)");
+
+const addOutdatedTokenFunction = process.env.REDIS ? __redisAddOutdatedToken : __localAddOutdatedToken;
+const isOutdatedTokenFunction = process.env.REDIS ? __redisIsOutdatedToken : __localIsOutdatedToken;
+
+async function isValidToken(token: string) {
+    if(await isOutdatedTokenFunction(token)) return false;
+    try {
+        const [_header, payload] = JWT.unwrap(token, secret);
+        return payload.exp > unixTime();
+    } catch(error) {
+        const errorMessage = (error as Error).message;
+        if(errorMessage === "Token signature does not match header and body.")
+            return false;
+        if(errorMessage === "Invalid JSON Web Token format.")
+            return false;
+        
+        metaLog("authentication", "ERROR",
+        `Encountered unknown error "${errorMessage}" while validating token "${token}". Returned false.`);
+        return false;
+    }
+}
+
+function outdateToken(token: string, expireAt: number) {
+    addOutdatedTokenFunction(token, expireAt);
+}
+
+function createToken(vaults: string[]) {
+    const current = unixTime();
+    const jwt = new JWT(issuer, current + tokenExpirationTime, current);
+    jwt.addClaim("vaults", vaults);
+    jwt.addClaim("nonce", Math.floor(Math.random() * 4294967295));
+    const token = jwt.getToken(secret);
+    metaLog("authentication", "INFO",
+    `Created new token ${token}. (Vaults: ${vaults}, Expiration: ${current + tokenExpirationTime})`);
+    return token;
+}
+
+// Returns a new token (with updated expirations) with the vault added.
+// MUST VERIFY TOKEN IS VALID before calling this function.
+function addNewVaultToToken(token: string, vault: string) {
+    const [_header, payload] = JWT.unwrap(token, secret);
+    const currentVaults = payload.vaults;
+    if(!currentVaults.includes(vault))
+        currentVaults.push(vault);
+    outdateToken(token, payload.exp);
+    return createToken(currentVaults);
+}
+
+// Returns a new token (with updated expirations) with the vault removed.
+// MUST VERIFY TOKEN IS VALID before calling this function.
+// A token with an empty array as its "vaults" field is safe.
+function removeVaultFromToken(token: string, vault: string) {
+    const [_header, payload] = JWT.unwrap(token, secret);
+    const currentVaults: string[] = payload.vaults;
+    const index = currentVaults.indexOf(vault);
+    if(index !== -1) currentVaults.splice(index, 1);
+    outdateToken(token, payload.exp);
+    return createToken(currentVaults);
+}
+
+// Issues a new token with updated expiration.
+// MUST VERIFY TOKEN IS VALID before calling this function.
+// Useful for short lived expirations for extra security, but refreshes
+// every time the user logs back in / accesses vault.
+function refreshTokenExpiration(token: string) {
+    const [_header, payload] = JWT.unwrap(token, secret);
+    const vaults = payload.vaults;
+    outdateToken(token, payload.exp);
+    return createToken(vaults);
+}
+
+export { isValidToken, createToken, addNewVaultToToken, removeVaultFromToken, outdateToken , refreshTokenExpiration };
