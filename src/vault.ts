b5726ed07a05480c97143cdac77cb5445656d049
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-03-03 19:57:41 -0500

    createNewVault() rewrite New error handling by returning CustomError

--- src/vault.ts
+++ src/vault.ts
@@ -11,21 +11,44 @@ import path from "path";
 import { metaLog } from "./logger.js";
 import { deleteVaultPassword, setVaultPassword, vaultExists } from "./authentication.js";
 
+import CustomError from "./custom_error.js";
+
 const baseVaultDirectory = process.env.VAULT_DIRECTORY || path.join(process.cwd(), "vaults");
 const baseVaultLoggingDirectory = process.env.LOGGING_DIRECTORY || path.join(process.cwd(), "logs", "vaults");
 // Must create corresponding log folder with creation of vault.
 // (The vaultLog function expects the folder to be present or error)
 
 
-// Password should be passed in as plain text. It will be hashed.
-// Vault name should be valid (consist only of base64url characters), or error is thrown
-// Vault should not already exist, or error is thrown
-// Vault logging directory may exist, as previously deleted vaults will retain their logging
-// directory for debugging and security purposes.
-// Returns true on success, will throw an error on failure, so expect true to always return.
-async function createNewVault(vaultName: string, password: string) {
+/**
+ * Password should be passed in as plain text. It will be hashed.
+ * 
+ * Vault name should be valid (consisting only of base64url characters), or an error
+ * is returned.
+ * (`INVALID_NAME`)
+ * 
+ * Vault should not already exist, or an error is returned.
+ * (`VAULT_DIRECTORY_EXISTS`)
+ * 
+ * Vault logging directory may exist, as previously deleted vaults will retain their
+ * logging directory for debugging and security purposes. If that happens, the creation
+ * will proceed, but an INFO message will be returned.
+ * (`LOGGING_DIRECTORY_EXISTS`)
+ * 
+ * If an unknown error is encountered, that error will be returned.
+ * 
+ * Returns any errors / infos if any were encountered, or null if everything goes successfully.
+ * 
+ * @param vaultName 
+ * @param password 
+ * @returns Promise of instance of CustomError with `code` and `type` attribute
+ */
+async function createNewVault(vaultName: string, password: string): Promise<CustomError | null> {
     if(!/^([a-z]|[A-Z]|[0-9]|_|-)+$/.test(vaultName)) {
-        throw new Error(`${vaultName} is not a valid vault name. The name may only consist of uppercase letters, lowercase letters, numbers, underscores (_), and dashes (-).`);
+        return new CustomError(
+            `${vaultName} is not a valid vault name. The name may only consist of uppercase letters, lowercase letters, numbers, underscores (_), and dashes (-).`,
+            "ERROR",
+            "INVALID_NAME"
+        );
     }
     try {
         await fs.mkdir(path.join(baseVaultDirectory, vaultName));
@@ -35,12 +58,23 @@ async function createNewVault(vaultName: string, password: string) {
         if(code === "EEXIST") {
             metaLog("admin", "ERROR",
             `Tried to create new vault directory ${vaultName} inside ${baseVaultDirectory}, but directory already exists. Aborting...`);
+            return new CustomError(
+                message,
+                "ERROR",
+                "VAULT_DIRECTORY_EXISTS"
+            );
         } else {
             metaLog("admin", "ERROR",
             `Tried to create new vault directory ${vaultName} inside ${baseVaultDirectory}, but encountered unrecognized error "${message}" instead. Aborting...`);
+            return new CustomError(
+                message,
+                "ERROR",
+                code || "NO_ERROR_CODE"
+            );
         }
-        throw error;
     }
+
+    let returnValue: CustomError | null = null;
     try {
         await fs.mkdir(path.join(baseVaultLoggingDirectory, vaultName));
     } catch(error) {
@@ -49,15 +83,17 @@ async function createNewVault(vaultName: string, password: string) {
         if(code === "EEXIST") {
             metaLog("admin", "INFO",
             `Tried to create logging directory for newly created vault ${vaultName} inside ${baseVaultLoggingDirectory}, but directory already exists. This may be because the logging directory was already created from a previous creation of this same vault.`);
+            returnValue = new CustomError(message, "INFO", "LOGGING_DIRECTORY_EXISTS");
         } else {
             metaLog("admin", "ERROR",
             `Tried to create logging directory for newly created vault ${vaultName} inside ${baseVaultLoggingDirectory}, but encountered unrecognized error "${message}" instead. Please check if the logging directory has been created, and if not, create it manually.`);
+            returnValue = new CustomError(message, "ERROR", code || "NO_ERROR_CODE");
         }
     }
     await setVaultPassword(vaultName, password);
     metaLog("admin", "INFO",
     `Created new vault ${vaultName} in ${baseVaultDirectory} and logging directory in ${baseVaultLoggingDirectory}.`);
-    return true;
+    return returnValue;
 }
 
 
