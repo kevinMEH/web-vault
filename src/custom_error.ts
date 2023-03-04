12146cae98317ea867bf98968d0184ea2f670c90
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-03-03 19:52:43 -0500

    CustomError class

---
+++ src/custom_error.ts
@@ -0,0 +1,16 @@
+class CustomError extends Error implements NodeJS.ErrnoException {
+    isCustom = true;
+    type: "ERROR" | "WARNING" | "INFO";
+    /**
+     * NO_ERROR_CODE if no error code.
+     */
+    code: string;
+
+    constructor(message: string, type: "ERROR" | "WARNING" | "INFO", code: string) {
+        super(message);
+        this.type = type;
+        this.code = code;
+    }
+}
+
+export default CustomError;
