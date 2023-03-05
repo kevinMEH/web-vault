088f5d4915d7d70cc7c110a2d8abf4a709f1838f
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-03-05 02:27:30 -0500

    CustomError code parameter optional

--- src/custom_error.ts
+++ src/custom_error.ts
@@ -1,15 +1,13 @@
 class CustomError extends Error implements NodeJS.ErrnoException {
     isCustom = true;
     type: "ERROR" | "WARNING" | "INFO";
-    /**
-     * NO_ERROR_CODE if no error code.
-     */
+    // NO_ERROR_CODE if no error code.
     code: string;
 
-    constructor(message: string, type: "ERROR" | "WARNING" | "INFO", code: string) {
+    constructor(message: string, type: "ERROR" | "WARNING" | "INFO", code?: string) {
         super(message);
         this.type = type;
-        this.code = code;
+        this.code = code || "NO_ERROR_CODE";
     }
 }
 
