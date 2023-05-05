85f16e55ac9a4935f92ad96fb56261cc8507f4e6
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-05-05 19:07:49 -0400

    Removed useless functions in JWT

--- src/authentication/jwt.ts
+++ src/authentication/jwt.ts
@@ -1,4 +1,4 @@
-import { createCipheriv, createDecipheriv, createHmac, randomFillSync } from "crypto";
+import { createHmac } from "crypto";
 import CustomError from "../custom_error.js";
 import { unixTime } from "../helper.js";
 
@@ -75,35 +75,6 @@ class JWT {
         return this.token;
     }
     
-    static algorithm = "aes-256-cbc";
-    /**
-     * Encrypts token using AES. 1st part of token will be the initialization vector,
-     * 2nd part will be the encrypted token. Parts separated by a ".".
-     * 
-     * @param aesKey Compact hex string representation of the AES key.
-     * @param secret Optional secret if the token has not been finalized yet.
-     * @returns 
-     */
-    getEncryptedToken(aesKey: string, secret?: string): string {
-        if(this.token === "") {
-            if(secret === undefined) {
-                throw new Error("The JSON Web Token has not been finalized yet.");
-            } else {
-                this.finalize(secret);
-            }
-        }
-        if(!(/^[a-fA-F0-9]+$/.test(aesKey)) || aesKey.length != 64) { // Using AES 256
-            throw new Error("AES key must be a hex string length 64 (256 bits). (No 0x)");
-        }
-        // AES block size 16 bytes
-        const initVector = randomFillSync(new Uint8Array(16));
-        const cipher = createCipheriv(JWT.algorithm, Buffer.from(aesKey, "hex"), initVector);
-        const encodedToken = toBase64(this.token); // We have to encode again because of the "." in the token. (Not valid base64url)
-        
-        return Buffer.from(initVector).toString("base64url") + "." + cipher.update(encodedToken, "base64url", "base64url") + cipher.final("base64url");
-    }
-    
-
     /**
      * Given a secret, verifies the JWT.
      * 
@@ -161,56 +132,6 @@ class JWT {
         const parts = token.split(".");
         return [objectFromBase64(parts[0]) as Header, objectFromBase64(parts[1]) as Payload];
     }
-    
-    /**
-     * Token will be decrypted, verified, and unwrapped.
-     * 
-     * Header and payload structure will not be verified.
-     * 
-     * Expiration will not be checked.
-     * 
-     * Returns the Header and Payload on success.
-     * 
-     * Returns null if the token does not verify. Ex: invalid
-     * format, bad initialization vector.
-     * 
-     * Throws an error on invalid AES key. Error means setup error.
-     * 
-     * Throws an error on invalid secret. Error means setup error.
-     * 
-     * @param encryptedToken 
-     * @param aesKey Must be 64 character hex string
-     * @param secret Must be hex string
-     * @returns 
-     */
-    static unwarpEncrypted(encryptedToken: string, aesKey: string, secret: string): [Header, Payload] | null {
-        if(!(/^[a-fA-F0-9]+$/.test(aesKey)) || aesKey.length != 64) { // Using AES 256
-            throw new Error("AES key must be a hex string length 64 (256 bits). (No 0x)");
-        }
-        
-        const encryptedParts = encryptedToken.split(".");
-        if(encryptedParts.length !== 2) {
-            return null;
-        }
-
-        const initVector = Uint8Array.from(Buffer.from(encryptedParts[0], "base64url"));
-        if(initVector.length !== 16) {
-            return null;
-        }
-
-        const decipher = createDecipheriv(JWT.algorithm, Buffer.from(aesKey, "hex"), initVector);
-        const encodedToken = decipher.update(encryptedParts[1], "base64url", "base64url") + decipher.final("base64url");
-        const token = fromBase64(encodedToken);
-        return this.unwrap(token, secret);
-    }
-}
-
-function toBase64(string: string) {
-    return Buffer.from(string).toString("base64url");
-}
-
-function fromBase64(string: string) {
-    return Buffer.from(string, "base64url").toString();
 }
 
 function objectToBase64(object: object) {
