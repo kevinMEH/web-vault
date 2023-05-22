2c5d43edb1b202e7dcdf7cc5190676ab5b29e97f
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-05-21 21:38:32 -0400

    TextField styling and error text

--- components/TextField.tsx
+++ components/TextField.tsx
@@ -1,4 +1,4 @@
-import type { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 
 type TextFieldParameters = {
     name: string;
@@ -7,35 +7,57 @@ type TextFieldParameters = {
     type: "email" | "password" | "text";
     value: string;
     setValue: Dispatch<SetStateAction<string>>;
+    id: string;
+
+    error?: string;
 
     inputMode?: "text" | "email";
     disabled?: boolean;
+    required?: boolean;
     readOnly?: boolean;
     
     className?: string;
 }
 
 const TextField = ({
-    name, labelText, placeholder, type, value, setValue,
-    inputMode = "text", disabled = false, readOnly = false,
+    name, labelText, placeholder, type, value, setValue, id, error = "",
+    inputMode = "text", disabled = false, required = false, readOnly = false,
     className = ""
 }: TextFieldParameters) => {
+    const [lastError, setLastError] = useState("");
+    const [newError, setNewError] = useState(true);
+    if(error != lastError) {
+        setLastError(error);
+        setNewError(true);
+    }
+
     return(
         <div className={"relative w-80 " + className}>
-            <label className="absolute font-inter text-xs text-sub left-4 -top-2 bg-white border-x-4 border-white">{labelText}</label>
             <input
                 name={name}
                 placeholder={placeholder}
                 type={type}
                 value={value}
-                onChange={event => setValue(event.target.value)}
+                onChange={event => {
+                    setNewError(false);
+                    setValue(event.target.value)
+                }}
+                id={id}
     
                 inputMode={inputMode}
                 disabled={disabled}
+                required={required}
                 readOnly={readOnly}
                 
-                className="px-5 py-3 w-full border border-gray rounded-md font-inter text-sm"
+                className={`px-5 py-3 w-full rounded-md text-sm peer
+                font-inter text-main ${error && newError ? "!text-error-light" : ""}
+                border border-gray ${error && newError ? "!border-error-light" : ""}
+                focus:border-accent-light`}
             />
+            <label htmlFor={id} className={`absolute font-inter text-xs text-sub left-4 -top-2 bg-white
+            ${error && newError ? "!text-error-light" : ""} peer-focus:text-accent-light
+            border-x-4 border-white`}>{labelText}</label>
+            { error ? <p className="inline-block pt-1.5 text-sm text-error-light">{error}</p> : null }
         </div>
     )
 }
