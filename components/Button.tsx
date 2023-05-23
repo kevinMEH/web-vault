84a16c8fc484ddca20a2c840987c4483a85f60eb
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-05-23 02:43:20 -0400

    Button component

---
+++ components/Button.tsx
@@ -0,0 +1,24 @@
+type ButtonParameter = {
+    children: React.ReactNode;
+
+    href?: string;
+    onClick?: (event: MouseEvent) => any;
+    disabled?: boolean;
+
+    wrapperClassName?: string;
+    className?: string;
+}
+
+const defaultClassNames = `justify-center inline-flex rounded-lg`;
+
+const Button = ({ children, href, onClick = undefined, disabled = false, wrapperClassName = "", className = "" }: ButtonParameter) => {
+    return <div className={wrapperClassName}>
+        {href ? <a href={href} onClick={onClick as any} className={defaultClassNames + " " + className}>
+            {children}
+        </a> : <button disabled={disabled} onClick={onClick as any} className={defaultClassNames + " " + className}>
+            {children}
+        </button>}
+    </div>
+}
+
+export default Button;
