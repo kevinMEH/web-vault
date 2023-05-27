b95007802376f7cfd4f794b74414d944a90e7f63
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-05-27 00:52:14 -0400

    ItemIcon from item name

---
+++ components/ItemIcon.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { memo } from "react";
+
+import { fileExtensionMap, fileNameMap, folderBaseMap } from "../src/icons/iconMap";
+
+type ItemIconParameters = {
+    name: string;
+    isFolder: boolean;
+    isOpen: boolean;
+    width: number;
+    height: number;
+    className: string;
+}
+
+// Takes an item name, and then returns the material icon SVG for the item.
+const ItemIcon = memo(function ItemIcon({ name, isFolder, isOpen, width, height, className }: ItemIconParameters) {
+    let svgName: string | undefined;
+    if(isFolder) {
+        svgName = folderBaseMap.get(name);
+        if(svgName !== undefined) {
+            svgName += (isOpen ? + "-open.svg" : ".svg");
+        } else {
+            svgName = isOpen ? "folder-open.svg" : "folder.svg";
+        }
+    } else {
+        svgName = fileNameMap.get(name);
+        if(svgName === undefined) {
+            const dotIndex = name.lastIndexOf(".");
+            if(dotIndex !== -1) {
+                svgName = fileExtensionMap.get(name.substring(dotIndex + 1));
+            }
+            if(svgName === undefined) {
+                svgName = "file.svg"
+            }
+        }
+    }
+    return <Image alt={name + " icon"} src={`/icons/${svgName}`} width={width} height={height} className={`inline-block ${className}`} />
+});
+
+export default ItemIcon
