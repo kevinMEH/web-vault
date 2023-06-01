afad00983f31009aca45946b13c114501f682851
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-06-01 17:42:06 -0400

    Changing activeDirectoryChain The active directory chain represents the path from the vault directory to the active directory, which is the directory used by the managers.

--- components/ExplorerItem.tsx
+++ components/ExplorerItem.tsx
@@ -1,59 +1,101 @@
-import { memo, useState } from "react";
-import { File, Directory } from "../src/vfs";
+import { memo, useCallback, useEffect, useRef, useState } from "react";
+
 import ItemIcon from "./ItemIcon";
 import Triangle from "./Triangle";
+import RightArrowSVG from "./RightArrowSVG";
+
+import { File, Directory } from "../src/vfs";
 
 type ExplorerItemParameters = {
     item: File | Directory;
     depth: number;
+    activeDirectoryChain: Directory[];
+    setActiveDirectoryChain: (setFunc: (prev: Directory[]) => Directory[]) => void;
 }
 
-const ExplorerItem = memo(function ExplorerItem({ item, depth }: ExplorerItemParameters) {
-    console.log("Rerendered " + item.name);
+const ExplorerItem = memo(function ExplorerItem({ item, depth, activeDirectoryChain, setActiveDirectoryChain }: ExplorerItemParameters) {
     const [_nonce, rerender] = useState(0);
+    const thisItem = useRef(null as null | HTMLElement)
 
     const isFolder = item.isDirectory;
     const isOpen = item.isOpen;
+    
+    const handleNewActiveDirectory = useCallback((event: CustomEvent) => {
+        event.detail.directoryChain[depth] = item;
+        if(depth === 1) {
+            event.stopPropagation();
+            event.preventDefault();
+            setActiveDirectoryChain(() => {
+                return event.detail.directoryChain;
+            });
+        }
+    }, [depth, item, setActiveDirectoryChain]);
+    
+    useEffect(() => {
+        const currentItem = thisItem?.current;
+        if(item.isDirectory) {
+            currentItem?.addEventListener("new_active_directory", handleNewActiveDirectory as (_: Event) => void);
+            return () => {
+                currentItem?.removeEventListener("new_active_directory", handleNewActiveDirectory as (_: Event) => void);
+            }
+        }
+    }, [handleNewActiveDirectory, item])
 
     const icon = <ItemIcon name={item.name} isFolder={isFolder} isOpen={isOpen}
         width={20} height={20} className="" />
-    return <div className="block" onClick={event => {
+
+    return <div className="block" ref={thisItem as any} onClick={event => {
         event.preventDefault();
         event.stopPropagation();
         item.isOpen = !item.isOpen;
         rerender(prev => prev + 1);
     }}>
-        <div className="flex items-center h-9 text-sub text-sm font-mono font-[425] hover:bg-light-gray cursor-pointer"
+        <div className="flex items-center h-9 text-quiet hover:text-main text-sm font-mono font-[425] bg-white hover:bg-light-gray transition-colors cursor-pointer justify-between group"
         style={{ paddingLeft: 12 * depth + "px"}} >
-            {
-                isFolder
-                ? (
-                    isOpen
-                    ? <Triangle transparentX={true} borderSizes="border-x-4 border-t-4 border-t-quiet" className="mx-1.5" />
-                    : <Triangle transparentX={false} borderSizes="border-y-4 border-l-4 border-l-quiet" className="mx-2" />
-                )
-                : <div className="min-w-[4px] mx-2" />
-            }
-            {icon}
-            <span className="pl-3">{item.name}</span>
-        </div>
-        {
-            isOpen && isFolder && <div>
+            <div className="flex items-center">
                 {
-                    (item as Directory).contents.sort((first, second) => {
-                        if(first.isDirectory != second.isDirectory) {
-                            return first.isDirectory ? -1 : 1;
-                        } else {
-                            return first.name.localeCompare(second.name);
-                        }
-                    })
-                    && (item as Directory).contents.map(
-                        item => <ExplorerItem item={item} depth={depth + 1} key={item.name} />
+                    isFolder
+                    ? (
+                        isOpen
+                        ? <Triangle transparentX={true} borderSizes="border-x-4 border-t-4 border-t-quiet" className="mx-1.5" />
+                        : <Triangle transparentX={false} borderSizes="border-y-4 border-l-4 border-l-quiet" className="mx-2" />
                     )
+                    : <div className="min-w-[4px] mx-2" />
                 }
+                {icon}
+                <span className="pl-3">{item.name}</span>
             </div>
-        }
+            {isFolder && <div className={`relative h-5 w-5 mr-3 flex-shrink-0`}>
+                <RightArrowSVG name="Set active directory" className={`text-quiet opacity-0 group-hover:opacity-100 hover:text-accent-dark transition-all
+                ${item === activeDirectoryChain[activeDirectoryChain.length - 1] ? "!opacity-100 !text-accent-dark" : ""}`}
+                onClick={event => {
+                    event.target?.dispatchEvent(new CustomEvent("new_active_directory", {
+                        bubbles: true, cancelable: true, detail: {
+                            directoryChain: [activeDirectoryChain[0]]
+                        }
+                    }));
+                    event.preventDefault();
+                    event.stopPropagation();
+                }} />
+            </div>}
+        </div>
+        {isOpen && isFolder && (item as Directory).contents.sort((first, second) => {
+            if(first.isDirectory != second.isDirectory) {
+                return first.isDirectory ? -1 : 1;
+            } else {
+                return first.name.localeCompare(second.name);
+            }
+        }) && <div>
+            {(item as Directory).contents.map(
+                item => <ExplorerItem item={item} depth={depth + 1} key={item.name} activeDirectoryChain={activeDirectoryChain} setActiveDirectoryChain={setActiveDirectoryChain} />
+            )}
+        </div>}
     </div>
+}, (previous, current) => {
+    return previous.item === current.item &&
+    previous.depth === current.depth &&
+    current.item !== previous.activeDirectoryChain[current.depth] &&
+    current.item !== current.activeDirectoryChain[current.depth];
 });
 
 export default ExplorerItem;
