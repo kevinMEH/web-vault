c9669e08b386a4c8ee991a424d2cba6311e55ab5
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-06-06 17:04:48 -0400

    TileView and Tiles

---
+++ frames/Managers/Tile.tsx
@@ -0,0 +1,28 @@
+import ItemIcon from "../../components/ItemIcon";
+
+import { File, Directory } from "../../src/vfs";
+
+type TileParameters = {
+    item: File | Directory;
+    setActiveDirectoryChain: (setFunc: (prev: Directory[]) => Directory[]) => void;
+    setActiveItem: (item: File | Directory) => void;
+}
+
+const Tile = ({ item, setActiveDirectoryChain, setActiveItem }: TileParameters) => {
+    return <div className="bg-white hover:bg-light-gray border border-gray rounded-lg px-5 py-3.5
+    flex items-center cursor-pointer select-none" onDoubleClick={item.isDirectory ? (() => {
+        setActiveDirectoryChain(prev => {
+            const newDirectoryChain = [...prev, item as Directory];
+            return newDirectoryChain;
+        });
+    }) : (() => {
+        setActiveItem(item);
+    })}>
+        <ItemIcon name={item.name} isFolder={item.isDirectory} isOpen={false} width={22} height={22} className="" />
+        <div className="pl-3 font-mono text-sm text-sub font-[425] w-full">
+            {item.name}
+        </div>
+    </div>
+}
+
+export default Tile;
---
+++ frames/Managers/TileView.tsx
@@ -0,0 +1,45 @@
+import Tile from "./Tile";
+
+import { File, Directory } from "../../src/vfs";
+import React from "react";
+
+type TileViewParameters = {
+    activeDirectoryChain: Directory[];
+    setActiveDirectoryChain: (setFunc: (prev: Directory[]) => Directory[]) => void;
+    setActiveItem: (item: File | Directory) => void;
+}
+
+const TileView = ({ activeDirectoryChain, setActiveDirectoryChain, setActiveItem }: TileViewParameters) => {
+    const paths = activeDirectoryChain.map(directory => directory.name);
+    const activeDirectory = activeDirectoryChain[activeDirectoryChain.length - 1];
+    return <div className="bg-light-gray h-full w-full px-6">
+        <div className="py-3.5 space-x-1 font-mono text-quiet text-sm font-medium">{
+        paths.map((path, i) => {
+            return i !== paths.length - 1
+            ? <React.Fragment key={i}>
+                <div className="inline-block cursor-pointer hover:bg-half-gray py-1 px-2.5 rounded-md" onClick={() => {
+                    setActiveDirectoryChain(prev => {
+                        const newDirectoryChain = [...prev];
+                        newDirectoryChain.length = i + 1;
+                        return newDirectoryChain;
+                    });
+                }}>{path}</div>
+                <div className="inline-block">/</div>
+                </React.Fragment> : <React.Fragment key={i}>
+                <div className="text-accent-dark inline-block cursor-pointer hover:bg-half-gray py-1 px-2.5 rounded-md" onClick={() => {
+                    setActiveDirectoryChain(prev => {
+                        const newDirectoryChain = [...prev];
+                        newDirectoryChain.length = i + 1;
+                        return newDirectoryChain;
+                    });
+                }}>{path}</div>
+            </React.Fragment>})
+        }</div>
+        <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(225px,1fr))]">{
+        activeDirectory.contents.map(item =>
+            <Tile item={item} setActiveDirectoryChain={setActiveDirectoryChain} setActiveItem={setActiveItem} key={item.name}></Tile>
+        )}</div>
+    </div>
+}
+
+export default TileView;
