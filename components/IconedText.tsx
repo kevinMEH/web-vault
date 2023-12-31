262b65a2b2b05ad6b91d5c66813f6950f07601ab
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-12-30 20:10:35 -0500

    IconedText.tsx Styling

--- components/IconedText.tsx
+++ components/IconedText.tsx
@@ -6,9 +6,9 @@ type IconedText = {
 };
 
 const IconedText = ({ IconFunction, text, iconName, wrapperClassName = "" }: IconedText) => {
-    return <div className={"space-x-1 " + wrapperClassName}>
+    return <div className={"space-x-1.5 " + wrapperClassName}>
         <IconFunction name={iconName} className="inline-block h-4 w-4" />
-        <span className="align-text-top">
+        <span className="align-text-top select-none">
         { text }
         </span>
     </div>
