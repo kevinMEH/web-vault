0ceafcd184429eb4e50cea8e6cc61d738ef83f94
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-12-30 20:09:50 -0500

    activity.svg

---
+++ components/SVGS/ActivitySVG.tsx
@@ -0,0 +1,21 @@
+type ActivitySVG = {
+    name: string;
+    className?: string;
+};
+
+const ActivitySVG = ({ name, className = "" }: ActivitySVG) => (
+    <svg name={name} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 28"
+        className={"relative, inset-0 " + className}
+    >
+        <polyline
+            points="30 14 24 14 20 26 12 2 8 14 2 14"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="3"
+        />
+    </svg>
+);
+
+export default ActivitySVG;
