f554c998bcbc9391b14a2dbd0308ef6651278929
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2024-01-10 20:47:35 -0500

    Massive changes to token.ts to vault_token.ts

---
+++ src/authentication/vault_token.ts
@@ -0,0 +1,204 @@
+import JWT, { Header } from "jwt-km";
+import { metaLog } from "../logger";
+import { unixTime } from "../helper";
+import { isOutdatedToken, _addOutdatedToken, issuedAfterVaultNonce, vaultExistsDatabase } from "./database";
+
+import { JWT_EXPIRATION, DOMAIN, JWT_SECRET } from "../env";
+
+export type VaultAccess = {
+    vault: string,
+    issuedAt: number,
+    expiration: number
+};
+
+export type WebVaultPayload = {
+    iss: string,
+    exp: number,
+    iat: number,
+    access: VaultAccess[]
+}
+
+/**
+ * Goes through all checks, and returns [Header, Payload] if the token is
+ * valid. If the token is invalid, null is returned.
+ * 
+ * Checks if outdated, body hash consistency, not malformed, nonces consistent.
+ * 
+ * NOTE: This function automatically removes invalid vaults from the payload.
+ * (Issued before nonce, expired vaults, etc.)
+ * 
+ * @param token 
+ * @returns Promise<[Header, WebVaultPayload, Token] | [null, null, null]>
+ */
+async function getUnwrappedToken(token: string): Promise<[Header, WebVaultPayload] | null> {
+    if(await isOutdatedToken(token)) return null;
+    const unwrapped = JWT.unwrap(token, JWT_SECRET);
+    // Malformed or inconsistent
+    if(unwrapped === null) {
+        return null;
+    }
+    const [header, payload] = unwrapped as [Header, WebVaultPayload];
+    // If expired
+    if(payload.exp < unixTime()) {
+        return null;
+    }
+    for(let i = 0; i < payload.access.length; i++) {
+        const { vault, issuedAt, expiration } = payload.access[i];
+        if(expiration < unixTime()) {
+            payload.access.splice(i, 1);
+            i--;
+        } else if(await issuedAfterVaultNonce(vault, issuedAt) === false) {
+            payload.access.splice(i, 1);
+            i--;
+        }
+    }
+    return [header, payload as WebVaultPayload];
+}
+
+/**
+ * The vaults parameter may be modified by the function. It is strongly
+ * recommended to pass in a referenceless copy of vaults, or one which is going
+ * to be outdated.
+ * 
+ * @param vault
+ * @returns 
+ */
+async function createToken(vault: string): Promise<string | null> {
+    if(await vaultExistsDatabase(vault) === false) {
+        return null;
+    }
+    const current = unixTime();
+    const singleAccess: VaultAccess = {
+        vault: vault,
+        issuedAt: current,
+        expiration: current + JWT_EXPIRATION
+    };
+    const jwt = new JWT(DOMAIN, current + JWT_EXPIRATION, current);
+    jwt.addClaim("access", [ singleAccess ]);
+    const token = jwt.getToken(JWT_SECRET);
+    metaLog("authentication", "INFO", `Created new token ${token}. (Vault: ${vault}, Issued at: ${current}, Expiration: ${current + JWT_EXPIRATION})`);
+    return token;
+}
+
+/**
+ * Returns a new token (with updated expiration) with the new vault added if it
+ * isn't already present. The new vault's issuing and expiration will be reset.
+ * 
+ * If the new vault is successfully added, the input token is invalidated.
+ * 
+ * @param token
+ * @param vault 
+ * @returns New token as a base64url string, or old token if vault is already
+ *          accessible or if vault does not exist, or null if token is invalid.
+ */
+async function addNewVaultToToken(token: string, vault: string): Promise<string | null> {
+    const unwrappedToken = await getUnwrappedToken(token);
+    if(unwrappedToken === null) {
+        return null;
+    }
+    if(await vaultExistsDatabase(vault) === false) {
+        return null;
+    }
+    const [ _header, payload ] = unwrappedToken;
+    const vaultAccesses = payload.access;
+    for(let i = 0; i < vaultAccesses.length; i++) {
+        if(vaultAccesses[i].vault === vault) {
+            vaultAccesses.splice(i, 1);
+            i--;
+        }
+    }
+    await addOutdatedToken(token, payload.exp);
+
+    const current = unixTime();
+    vaultAccesses.push({
+        vault: vault,
+        issuedAt: current,
+        expiration: current + JWT_EXPIRATION
+    });
+    const jwt = new JWT(DOMAIN, current + JWT_EXPIRATION, current);
+    jwt.addClaim("access", vaultAccesses);
+    const newToken = jwt.getToken(JWT_SECRET);
+    metaLog("authentication", "INFO", `Adding new vault ${vault} to token ${token}, receiving new token ${newToken}`);
+    return newToken;
+}
+
+/**
+ * Returns a new token (with updated expiration) with the vault removed if it
+ * is present.
+ * 
+ * If the vault is successfully removed, the input token is invalidated.
+ * 
+ * A token with an empty array as its "vaults" field is safe. An invalid vault
+ * is safe.
+ * 
+ * @param token
+ * @param vault 
+ * @returns New token as a base64url string, or old token if vault was never
+ *          present, or null if token is invalid.
+ */
+async function removeVaultFromToken(token: string, vault: string): Promise<string | null> {
+    const unwrappedToken = await getUnwrappedToken(token);
+    if(unwrappedToken === null) {
+        return null;
+    }
+    const [ _header, payload ] = unwrappedToken;
+    const vaultAccesses = payload.access;
+    for(let i = 0; i < vaultAccesses.length; i++) {
+        if(vaultAccesses[i].vault === vault) {
+            vaultAccesses.splice(i, 1);
+            i--;
+        }
+    }
+    await addOutdatedToken(token, payload.exp);
+    
+    const current = unixTime();
+    const jwt = new JWT(DOMAIN, current + JWT_EXPIRATION, current);
+    jwt.addClaim("access", vaultAccesses);
+    const newToken = jwt.getToken(JWT_SECRET);
+    metaLog("authentication", "INFO", `Removed vault ${vault} from token ${token}, receiving new token ${newToken}`);
+    return newToken;
+}
+
+/**
+ * Returns a new token with updated expiration for the given vault. Input token
+ * is invalidated.
+ * 
+ * @param token
+ * @param vault
+ * @returns New token as a base64url string, or old token if vault is not 
+ *          present in the token, or null if token is invalid.
+ */
+async function _refreshVaultExpiration(token: string, vault: string): Promise<string | null> {
+    const unwrappedToken = await getUnwrappedToken(token);
+    if(unwrappedToken === null) {
+        return null;
+    }
+    const [ _header, payload ] = unwrappedToken;
+    const vaultAccesses = payload.access;
+    const current = unixTime();
+    for(const access of vaultAccesses) {
+        if(access.vault === vault) {
+            access.expiration = current + JWT_EXPIRATION;
+        }
+    }
+    await addOutdatedToken(token, payload.exp);
+
+    const jwt = new JWT(DOMAIN, current + JWT_EXPIRATION, current);
+    jwt.addClaim("access", vaultAccesses);
+    const newToken = jwt.getToken(JWT_SECRET);
+    metaLog("authentication", "INFO",`Refreshing token ${token} to new token ${newToken}`);
+    return newToken;
+}
+
+async function addOutdatedToken(token: string, expireAt: number) {
+    metaLog("authentication", "INFO", `Outdating token ${token}, expiring at ${expireAt}`);
+    await _addOutdatedToken(token, expireAt);
+}
+
+export {
+    getUnwrappedToken,
+    createToken,
+    addNewVaultToToken,
+    removeVaultFromToken,
+    _refreshVaultExpiration
+};
--- test/auth_tests.ts
+++ test/auth_tests.ts
@@ -11,9 +11,16 @@ if(process.env.REDIS) console.log("Using Redis");
 else console.log("Using in memory database");
 const { cleanup } = await import("../src/cleanup");
 
-const { default: JWT } = await import("jwt-km");
+import JWT from "jwt-km";
 import type { Header, Payload } from "jwt-km";
-const { getUnwrappedToken, createToken, addNewVaultToToken, removeVaultFromToken, refreshTokenExpiration } = await import("../src/authentication/token");
+import type { VaultAccess } from "../src/authentication/vault_token";
+const {
+    getUnwrappedToken,
+    createToken,
+    addNewVaultToToken,
+    removeVaultFromToken,
+    _refreshVaultExpiration
+} = await import("../src/authentication/vault_token");
 const {
     _addOutdatedToken,
     setVaultPassword,
@@ -26,100 +33,7 @@ const {
 } = await import("../src/authentication/database");
 
 describe("Authentication tests", () => {
-    describe("Testing token authentication module", () => {
-        it("Creates a new token and checks if successful by using the JWT class", async () => {
-            await setVaultPassword("main_vault", "password");
-            const token = await createToken(["main_vault"]);
-            const [_header, payload] = JWT.unwrap(token, process.env.JWT_SECRET as string) as [Header, Payload];
-            const token2 = new JWT(process.env.DOMAIN as string, payload.exp, payload.iat)
-                    .addClaim("vaults", ["main_vault"])
-                    .addClaim("nonces", [payload?.nonces[0]])
-                    .getToken(process.env.JWT_SECRET);
-            assert(token === token2);
-            assert(await getUnwrappedToken(token) !== null);
-            assert(await getUnwrappedToken(token2) !== null);
-            await deleteVaultPassword("main_vault");
-        });
-        
-        it("Checks that expired tokens and bad tokens are not valid", async () => {
-            await setVaultPassword("vault", "password");
-            const token = await createToken(["vault"]);
-            assert(await getUnwrappedToken(token) !== null);
-            assert(await getUnwrappedToken(token.substring(0, token.length - 1) as Token) === null);
-            
-            // Taken from main tests. exp was at 1000000000 unix time (Sept 9, 2001) (A long time ago)
-            const oldToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJLZXZpbiIsImV4cCI6MTAwMDAwMDAwMCwiaWF0IjoxMTExMTExMTExLCJpc3N1ZXJJc0Nvb2wiOnRydWV9.wm1_FGup-Jkj8b9_EtV0sLWc8Z-xkW2sIq0y48TaJiQ";
-            assert(await getUnwrappedToken(oldToken as Token) === null);
-            await deleteVaultPassword("vault");
-        });
-        
-        it("Outdates a token", async () => {
-            await setVaultPassword("log_me_out", "asdfasd");
-            const token = await createToken(["log_me_out"]);
-            const [_header, payload] = JWT.unwrap(token, process.env.JWT_SECRET as string) as [Header, Payload];
-            _addOutdatedToken(token, payload.exp);
-            assert(await getUnwrappedToken(token) === null);
-            await deleteVaultPassword("log_me_out");
-        });
-        
-        it("Creates a new token based on an old token with new vault", async () => {
-            await setVaultPassword("old_vault", "a--12n");
-            await setVaultPassword("new_vault", "a--12n");
-            const token = await createToken(["old_vault"]);
-            const unwrappedToken = await getUnwrappedToken(token) as any;
-            const newToken = await addNewVaultToToken(unwrappedToken, "new_vault");
-            const [_header, payload] = JWT.unwrap(newToken, process.env.JWT_SECRET as string) as [Header, Payload];
-            assert(payload.vaults.includes("old_vault"));
-            assert(payload.vaults.includes("new_vault"));
-            await deleteVaultPassword("old_vault");
-            await deleteVaultPassword("new_vault");
-        });
-        
-        it("Creates a new token based on an old token with a vault removed", async () => {
-            await setVaultPassword("remove_me", "asd2");
-            const token = await createToken(["remove_me"]);
-            const unwrappedToken = await getUnwrappedToken(token) as any;
-            const newToken = await removeVaultFromToken(unwrappedToken, "remove_me");
-            const [_header, payload] = JWT.unwrap(newToken, process.env.JWT_SECRET as string) as [Header, Payload];
-            assert(!payload.vaults.includes("remove_me"));
-            assert(!payload.vaults.includes("random"));
-            assert(!payload.vaults.includes(""));
-            await deleteVaultPassword("remove_me");
-        });
-        
-        it("Refreshes a token's expiration date", async () => {
-            await setVaultPassword("log_me_out", "asdfn");
-            const token = await createToken(["log_me_out"]);
-            const [_header, payload] = JWT.unwrap(token, process.env.JWT_SECRET as string) as [Header, Payload];
-            const unwrappedToken = await getUnwrappedToken(token) as any;
-            assert(unwrappedToken[0] !== null);
-            await new Promise(resolve => setTimeout(resolve, 2000));
-
-            const newToken = await refreshTokenExpiration(unwrappedToken);
-            const [_newHeader, newPayload] = JWT.unwrap(newToken, process.env.JWT_SECRET as string) as [Header, Payload];
-            assert(newPayload.exp > payload.exp);
-            assert(newPayload.iat > payload.iat);
-            assert(await getUnwrappedToken(token) === null);
-        });
-        
-        it("Changing a vault's password invalidates token as nonces change", async () => {
-            await setVaultPassword("change my password", "first password");
-            const token = await createToken(["change my password"]);
-            const unwrapped = await getUnwrappedToken(token);
-            assert(unwrapped !== null);
-            assert(unwrapped[1].vaults.length === 1);
-            assert(unwrapped[1].nonces.length === 1);
-            
-            await setVaultPassword("change my password", "second password");
-            const unwrappedAfter = await getUnwrappedToken(token);
-            assert(unwrappedAfter !== null);
-            assert(unwrappedAfter[1].vaults.length === 0);
-            assert(unwrappedAfter[1].nonces.length === 0);
-            await deleteVaultPassword("change my password");
-        });
-    });
-
-    describe("Testing vault authentication functions", () => {
+    describe("Testing database vault authentication functions (src/authentication/database.ts)", () => {
         it("Sets the password for a vault and checks if successful", async () => {
             await setVaultPassword("testing", "password123");
             assert(await verifyVaultPassword("testing", "password123"));
@@ -138,7 +52,7 @@ describe("Authentication tests", () => {
         });
     });
     
-    describe("Testing admin authentication functions", () => {
+    describe("Testing database admin authentication functions (src/authentication/database.ts)", () => {
         it("Sets the password for an admin and checks if successful", async () => {
             await setAdminPassword("some_admin", "some_password");
             assert(await _verifyAdminPassword("some_admin", "some_password"));
@@ -146,6 +60,127 @@ describe("Authentication tests", () => {
             assert(!await _verifyAdminPassword("nonexistant", "some_password"));
             await deleteAdminPassword("some_admin");
             assert(!await _verifyAdminPassword("some_admin", "some_password"));
+        });
+    });
+
+    describe("Vault token authentication tests (src/authentication/vault_token.ts)", () => {
+        it("Creates a new token and checks if successful by using the JWT class", async () => {
+            await setVaultPassword("main_vault", "password");
+
+            const token = await createToken("main_vault");
+            assert(token !== null);
+            const [_header, payload] = JWT.unwrap(token, process.env.JWT_SECRET as string) as [Header, Payload];
+            assert(payload.access?.length === 1);
+            assert(payload.access[0].vault === "main_vault");
+            const token2 = new JWT(process.env.DOMAIN as string, payload.exp, payload.iat)
+                    .addClaim("access", [{
+                        vault: "main_vault",
+                        issuedAt: payload.access[0].issuedAt,
+                        expiration: payload.access[0].expiration
+                    }]).getToken(process.env.JWT_SECRET as string);
+            assert(token === token2);
+            assert(await getUnwrappedToken(token2) !== null);
+
+            await deleteVaultPassword("main_vault");
+        });
+        
+        it("Checks that expired tokens and bad tokens are not valid", async () => {
+            await setVaultPassword("vault", "password");
+
+            const token = await createToken("vault");
+            assert(token !== null);
+            assert(await getUnwrappedToken(token) !== null);
+            assert(await getUnwrappedToken(token.substring(0, token.length - 1)) === null);
+            
+            // Taken from main tests. exp was at 1000000000 unix time (Sept 9, 2001) (A long time ago)
+            const oldToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJLZXZpbiIsImV4cCI6MTAwMDAwMDAwMCwiaWF0IjoxMTExMTExMTExLCJpc3N1ZXJJc0Nvb2wiOnRydWV9.wm1_FGup-Jkj8b9_EtV0sLWc8Z-xkW2sIq0y48TaJiQ";
+            assert(await getUnwrappedToken(oldToken) === null);
+
+            await deleteVaultPassword("vault");
+        });
+        
+        it("Outdates a token", async () => {
+            await setVaultPassword("log_me_out", "asdfasd");
+
+            const token = await createToken("log_me_out");
+            assert(token !== null);
+            const [_header, payload] = JWT.unwrap(token, process.env.JWT_SECRET as string) as [Header, Payload];
+            await _addOutdatedToken(token, payload.exp as number);
+            assert(await getUnwrappedToken(token) === null);
+
+            await deleteVaultPassword("log_me_out");
+        });
+        
+        it("Creates a new token based on an old token with new vault", async () => {
+            await setVaultPassword("old_vault", "a--12n");
+            await setVaultPassword("new_vault", "a--12n");
+
+            const token = await createToken("old_vault");
+            assert(token !== null);
+            assert(await getUnwrappedToken(token) !== null);
+            const newToken = await addNewVaultToToken(token, "new_vault");
+            assert(newToken !== null);
+            assert(await getUnwrappedToken(token) === null);
+            const [_header, payload] = JWT.unwrap(newToken, process.env.JWT_SECRET as string) as [Header, Payload];
+            assert(payload.access.some((access: VaultAccess) => access.vault === "old_vault"));
+            assert(payload.access.some((access: VaultAccess) => access.vault === "new_vault"));
+            
+            await deleteVaultPassword("old_vault");
+            await deleteVaultPassword("new_vault");
+        });
+        
+        it("Creates a new token based on an old token with a vault removed", async () => {
+            await setVaultPassword("remove_me", "asd2");
+
+            const token = await createToken("remove_me");
+            assert(token !== null);
+            assert(await getUnwrappedToken(token) !== null);
+            const newToken = await removeVaultFromToken(token, "remove_me");
+            assert(newToken !== null);
+            assert(await getUnwrappedToken(token) === null);
+            const [_header, payload] = JWT.unwrap(newToken, process.env.JWT_SECRET as string) as [Header, Payload];
+            assert(payload.access.length === 0);
+
+            await deleteVaultPassword("remove_me");
+        });
+        
+        it("Refreshes a token's expiration date", async () => {
+            await setVaultPassword("unique_name", "asdfn");
+
+            const token = await createToken("unique_name");
+            assert(token !== null);
+            assert(await getUnwrappedToken(token) !== null);
+            const [_header, payload] = JWT.unwrap(token, process.env.JWT_SECRET as string) as [Header, Payload];
+
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            const newToken = await _refreshVaultExpiration(token, "unique_name");
+
+            assert(newToken !== null);
+            assert(await getUnwrappedToken(token) === null);
+            const [_newHeader, newPayload] = JWT.unwrap(newToken, process.env.JWT_SECRET as string) as [Header, Payload];
+            assert(newPayload.exp > payload.exp);
+            assert(newPayload.iat > payload.iat);
+            
+            await deleteVaultPassword("unique_name");
+        });
+        
+        it("Changing a vault's password invalidates token as nonces change", async () => {
+            await setVaultPassword("change my password", "first password");
+
+            const token = await createToken("change my password");
+            assert(token !== null);
+            const unwrapped = await getUnwrappedToken(token);
+            assert(unwrapped !== null);
+            assert(unwrapped[1].access.length === 1);
+            
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            await setVaultPassword("change my password", "second password");
+
+            const unwrappedAfter = await getUnwrappedToken(token);
+            assert(unwrappedAfter !== null);
+            assert(unwrappedAfter[1].access.length === 0);
+
+            await deleteVaultPassword("change my password");
         });
     });
 
