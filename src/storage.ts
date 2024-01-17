ced8e81000fb3f5d84fb31cea8d35217dd983c03
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2024-01-17 18:07:03 -0500

    Added vault token storage

--- src/storage.ts
+++ src/storage.ts
@@ -1,9 +1,17 @@
 // Frontend only
 
-export function setAdminToken(vaultToken: string) {
-    localStorage.setItem("admin_token", vaultToken); // eslint-disable-line
+export function setAdminToken(adminToken: string) {
+    localStorage.setItem("admin_token", adminToken); // eslint-disable-line
 }
 
 export function getAdminToken(): string | null {
     return localStorage.getItem("admin_token"); // eslint-disable-line
+}
+
+export function setVaultToken(vaultToken: string) {
+    localStorage.setItem("vault_token", vaultToken); // eslint-disable-line
+}
+
+export function getVaultToken() {
+    return localStorage.getItem("vault_token"); // eslint-disable-line
 }
