886d980da9e99bd7694a825dcceefedd1c21ed01
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2024-01-29 00:11:59 -0500

    api/file/vfs now accepts any valid destination

--- app/api/file/vfs/route.ts
+++ app/api/file/vfs/route.ts
@@ -2,7 +2,7 @@
 
 import { NextRequest } from "next/server";
 import { SimpleDirectory } from "../../../../src/vfs";
-import { getDirectoryAt, validatePath } from "../../../../src/controller";
+import { getDirectoryAt, validateDestination } from "../../../../src/controller";
 import { Answer, AuthResponse, badParameters, ErrorResponse, WithSinglePathAuthentication } from "../../../../src/route_helpers";
 import { MAX_VFS_DEPTH, DEFAULT_VFS_DEPTH } from "../../../../src/env";
 
@@ -16,12 +16,13 @@ export type Data = {
     directory: SimpleDirectory | undefined
 } | ErrorResponse;
 
+
 export function POST(request: NextRequest): Promise<AuthResponse<Data>> {
     return WithSinglePathAuthentication<Data>(request, (body, path) => {
-        const validPath = validatePath(path);
-        if(validPath === null) {
+        const validDestination = validateDestination(path);
+        if(validDestination === null) {
             return Promise.resolve(Answer<ErrorResponse>(400, {
-                error: badParameters("The provided path is not valid.")
+                error: badParameters("The provided path is not a valid destination.")
             }));
         }
         const userDepth = body.depth || DEFAULT_VFS_DEPTH;
@@ -29,11 +30,10 @@ export function POST(request: NextRequest): Promise<AuthResponse<Data>> {
             return Promise.resolve(Answer<ErrorResponse>(400, {
                 error: badParameters("Expected body with optional number attribute depth.")
             }));
-        } else {
-            const depth = userDepth < 0 ? DEFAULT_VFS_DEPTH : Math.min(userDepth, MAX_VFS_DEPTH);
-            return Promise.resolve(Answer(200, {
-                directory: getDirectoryAt(validPath)?.flat(false, depth)
-            }));
         }
+        const depth = userDepth < 0 ? DEFAULT_VFS_DEPTH : Math.min(userDepth, MAX_VFS_DEPTH);
+        return Promise.resolve(Answer(200, {
+            directory: getDirectoryAt(validDestination)?.flat(false, depth)
+        }));
     });
 }
