3d330e92e0e642e9b73bea4e560a63ff35028709
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2024-02-05 23:03:16 -0500

    IconedText wrapperClassName renamed to className

--- components/IconedText.tsx
+++ components/IconedText.tsx
@@ -2,11 +2,11 @@ type IconedText = {
     IconFunction: (_: any) => JSX.Element;
     text: string;
     iconName: string;
-    wrapperClassName?: string;
+    className?: string;
 };
 
-const IconedText = ({ IconFunction, text, iconName, wrapperClassName = "" }: IconedText) => {
-    return <div className={"space-x-1.5 " + wrapperClassName}>
+const IconedText = ({ IconFunction, text, iconName, className = "" }: IconedText) => {
+    return <div className={"space-x-1.5 " + className}>
         <IconFunction name={iconName} className="inline-block h-4 w-4" />
         <span className="align-text-top select-none">
         { text }
