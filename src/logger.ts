4a4fbf92aa7637d7980c10dff1a76ce3ae9bbd57
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2024-05-17 23:43:43 -0400

    errorFormatter helper

--- src/logger.ts
+++ src/logger.ts
@@ -4,7 +4,11 @@ import { BASE_LOGGING_DIRECTORY } from "./env";
 
 import type { VaultPath } from "./controller";
 
-async function metaLog(name: "database" | "authentication" | "admin" | "vfs" | "file system" | "runtime" | "requests", type: "ERROR" | "WARNING" | "INFO", message: string) {
+export default function errorFormatter(error: Error) {
+    return `"${error.name}: ${error.message}"`;
+}
+
+export async function metaLog(name: "database" | "authentication" | "admin" | "vfs" | "file system" | "runtime" | "requests", type: "ERROR" | "WARNING" | "INFO", message: string) {
     const filePath = path.join(BASE_LOGGING_DIRECTORY, name, logFileNameFromDate());
     message = (new Date()).toUTCString() + "\n" + type + ": " + message + "\n\n";
     await fs.appendFile(filePath, message, { mode: 0o640, flag: "a" });
@@ -21,7 +25,7 @@ async function metaLog(name: "database" | "authentication" | "admin" | "vfs" | "
  * @param type 
  * @param message 
  */
-async function vaultLog(vaultName: VaultPath, type: "ERROR" | "NON URGENT" | "WARNING" | "INFO", message: string) {
+export async function vaultLog(vaultName: VaultPath, type: "ERROR" | "NON URGENT" | "WARNING" | "INFO", message: string) {
     const filePath = path.join(BASE_LOGGING_DIRECTORY, "vaults", vaultName, logFileNameFromDate());
     // TODO: Switch to file descriptors and have array of file
     // descriptors to read and write to. (Performance reasons)
@@ -34,9 +38,7 @@ async function vaultLog(vaultName: VaultPath, type: "ERROR" | "NON URGENT" | "WA
  * 
  * @returns file name string
  */
-function logFileNameFromDate(): string {
+export function logFileNameFromDate(): string {
     const date = new Date();
     return date.getUTCFullYear() + "_" + (1 + date.getUTCMonth()) + "_" + date.getUTCDate() + ".log";
 }
-
-export { metaLog, vaultLog, logFileNameFromDate };
